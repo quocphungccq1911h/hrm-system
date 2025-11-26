6cba0322fe313f7e6e42eb433490205a93e5c225
PHOENIX\USER <phungphan@wwct.vn>
2025-11-26 17:27:56 +0700

    yyy

--- auth-service/src/main/java/com/hrm/auth/mapper/ext/ExtUserMapper.java
+++ auth-service/src/main/java/com/hrm/auth/mapper/ext/ExtUserMapper.java
@@ -2,10 +2,14 @@ package com.hrm.auth.mapper.ext;
 
 import com.hrm.auth.model.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Mapper
 public interface ExtUserMapper {
-    Optional<User> findByUsername(String userName);
+    Optional<User> findByUsername(@Param("username") String userName);
+
+    Optional<User> findById(@Param("id") UUID id);
 }
